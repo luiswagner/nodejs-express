c6b8667c727d63013e430ddf9c4b353408df414b
luiswagner <luis.wagner@gmail.com>
2024-03-30 21:49:11 -0300

    Aula: 03 18/03/2024

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import { Client } from 'pg'
 
-app.get('/', (req, res) => {
-  res.send('Hello Codelab!')
-})
+async function main() {
+  const app = express()
+  const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port: ${port}`)
-})
+  const client = new Client()
+  await client.connect()
+   
+  const res = await client.query('SELECT $1::text as message', ['DB Connection is OK!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+    
+  app.get('/', (req, res) => {
+    res.send('Hello Codelab!!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port: ${port}`)
+  })
+
+  
+}
+//Chamando a função
+main()
